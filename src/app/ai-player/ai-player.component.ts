ba4c4deb315a436929a052903225281d67bc9af1
Ariel Perez <arielpgmm@gmail.com>
2022-09-11 17:44:18 -0400

    ai component improved

--- src/app/ai-player/ai-player.component.ts
+++ src/app/ai-player/ai-player.component.ts
@@ -1,4 +1,5 @@
-import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output, ViewChild } from '@angular/core';
+import { BoardComponent } from '../board/board.component';
 
 @Component({
   selector: 'app-ai-player',
@@ -15,9 +16,9 @@ export class AiPlayerComponent implements OnInit {
 
   private w: number = 0; // width / 3
   private h: number = 0; // height / 3
-  private aiPlay: string = '';
-  private human: string = 'circle'
-  private currentPlayer: string = this.human;
+  public aiPlay: string = '';
+  // private human: string = 'circle'
+  // private currentPlayer: string = this.human;
 
   constructor() { }
 
@@ -27,17 +28,19 @@ export class AiPlayerComponent implements OnInit {
   public getChange(): boolean {
     let toReturn: boolean = false;
     const count: number = this.getMarksCount();
+    if (this.aiPlay === '') {
+      this.aiPlay = this.play
+    }
     if (count !== this.boardLength && count !== 0) {
+      this.boardLength = count;
+
       setTimeout((): void => {
-        this.boardLength = count;
-        this.move.emit(this.getMove());
+        if (this.board.some(b=>b !== '') && this.play === this.aiPlay) {
+          const move: number = this.getMove();
+          this.move.emit(move);
+        }
+        toReturn = true;
       }, 500);
-      if (this.aiPlay === '') {
-        this.aiPlay = this.play
-      }
-      // console.log("this.board: \n", this.board);
-      // console.log(this.play);
-      toReturn = true;
     }
     return toReturn;
   }
@@ -55,8 +58,10 @@ export class AiPlayerComponent implements OnInit {
   }
 
   private getMove(): number {
-    let toReturn!: number;
-    toReturn = 8;
+    let toReturn: number = Math.floor(Math.random() * 9 );
+    if (this.board[toReturn] !== '') {
+      toReturn = this.getMove();
+    }
     return toReturn;
   }
 
@@ -104,8 +109,63 @@ export class AiPlayerComponent implements OnInit {
 
   }
 
+  // It considers all the possible ways the game can go and returns the value of the board
   private minimax(board: number, depth: number, isMaximizing: boolean): number {
     return 1;
+  }
+
+  private evaluate(board: string[]): number {
+    let toReturn: number = 0;
+
+    for (let row: number = 0; row < 3; row++) { // Checking for Rows for X or O victory.
+      if (board[row][0] === board[row][1] &&
+          board[row][1] === board[row][2]) {
+        if (board[row][0] === 'x') {
+          toReturn = +10;
+        } else {
+          toReturn = -10;
+        }
+      }
+    }
+
+    for (let col: number = 0; col < 3; col++) { // Checking for Columns for X or O victory.
+      if (board[0][col] === board[1][col] &&
+          board[1][col] === board[2][col]) {
+        if (board[0][col] === 'x') {
+          toReturn = +10;
+        } else {
+          toReturn = -10;
+        }
+      }
+    }
+
+    // Checking for Diagonals for X or O victory.
+    if (board[0][0] == board[1][1] && board[1][1] == board[2][2])
+    {
+        if (board[0][0] == 'x')
+            toReturn +10;
+        else
+            toReturn -10;
+    }
+    if (board[0][2] === board[1][1] &&
+        board[1][1] === board[2][0]) {
+      if (board[0][2] == 'x')
+          toReturn +10;
+      else
+          toReturn -10;
+    }
+
+    return toReturn; // If none have won return 0
+  }
+
+  private findBestMove(board: any): void { // This will return the best possible move for the player
+      let bestVal: number = -1000;
+      let bestMove: { row: number, col: number } = {
+        row: 0,
+        col: 0
+      };
+      bestMove.row = -1;
+      bestMove.col = -1;
   }
 
 }
